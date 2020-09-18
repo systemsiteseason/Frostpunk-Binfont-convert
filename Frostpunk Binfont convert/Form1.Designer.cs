8f07171b4884801c8daeb38b1d99c7503ce0d9cd
Son Hoang <haitac865@gmail.com>
2020-09-18 09:56:04 +0700

    commit

--- Frostpunk Binfont convert/Form1.Designer.cs
+++ Frostpunk Binfont convert/Form1.Designer.cs
@@ -28,16 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.btnConvert = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnConvert
             // 
-            this.btnConvert.Location = new System.Drawing.Point(354, 143);
+            this.btnConvert.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnConvert.Location = new System.Drawing.Point(342, 148);
             this.btnConvert.Name = "btnConvert";
-            this.btnConvert.Size = new System.Drawing.Size(75, 23);
+            this.btnConvert.Size = new System.Drawing.Size(99, 25);
             this.btnConvert.TabIndex = 0;
-            this.btnConvert.Text = "button1";
+            this.btnConvert.Text = "Convert/Cooked";
             this.btnConvert.UseVisualStyleBackColor = true;
             this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
             // 
@@ -45,10 +47,15 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(441, 178);
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(444, 176);
             this.Controls.Add(this.btnConvert);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Frostpunk Binfont";
             this.ResumeLayout(false);
 
         }
--- Frostpunk Binfont convert/Form1.cs
+++ Frostpunk Binfont convert/Form1.cs
@@ -27,60 +27,110 @@ namespace Frostpunk_Binfont_convert
             {
                 if (!string.IsNullOrEmpty(opf.FileName))
                 {
+                    string pth = Path.GetDirectoryName(opf.FileName);
+                    string filename = Path.GetFileNameWithoutExtension(opf.FileName);
                     if(Path.GetExtension(opf.FileName) == ".binfont")
                     {
+                        byte[] hexData = {
+                            0x44, 0x44, 0x53, 0x20, 0x7C, 0x00, 0x00, 0x00, 0x0F, 0x10, 0x02, 0x00 };
+                        byte[] nextData = {
+                            0x01, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00,
+                            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+                            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+                            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x20, 0x00, 0x00, 0x00,
+                            0x41, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x20, 0x00, 0x00, 0x00, 0x00, 0x00, 0xFF, 0x00,
+                            0x00, 0xFF, 0x00, 0x00, 0xFF, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xFF, 0x00, 0x10, 0x00, 0x00,
+                            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+                        };
+
                         var rd = new BinaryReader(File.OpenRead(opf.FileName));
                         int width = 4096;
                         int magic = rd.ReadInt32();
                         int ver = rd.ReadInt32();
                         int texs = rd.ReadInt32();
-                        for(int i = 0; i < texs; i++)
+                        byte[] height = rd.ReadBytes(texs * 4);
+                        Console.WriteLine("Version {0} , Textures {1}", ver, texs);
+                        var ms = new BinaryReader(new MemoryStream(height));
+                        for (int i = 0; i < texs; i++)
                         {
-
+                            int h = ms.ReadInt32();
+                            int Size = width * h * 4;
+                            byte[] outbuf = Dec(rd.ReadBytes(Size));
+                            Console.WriteLine("Dump file {0}", pth + "\\" + filename +"_tex_" + i.ToString() + ".dds");
+                            var wt = new BinaryWriter(File.Create(pth + "\\" + filename + "_tex_" + i.ToString() + ".dds"));
+                            wt.Write(hexData);
+                            wt.Write(h);
+                            wt.Write(width);
+                            wt.Write(Size);
+                            wt.Write(nextData);
+                            wt.Write(outbuf);
+                            wt.Flush();
+                            wt.Close();
+                            Console.WriteLine("Convert Done!");
                         }
+                        Console.WriteLine("Dump fnt {0}", pth + "\\" + filename + "_table.fnt");
+                        int count = rd.ReadInt32();
+                        StreamWriter fntwt = new StreamWriter(File.Create(pth + "\\" + filename + "_table.fnt"), Encoding.UTF8);
+                        for(int i = 0; i < count; i++)
+                        {
+                            float left = rd.ReadSingle();
+                            float right = rd.ReadSingle();
+                            float top = rd.ReadSingle();
+                            float bottom = rd.ReadSingle();
+                            float wd = rd.ReadSingle();
+                            float offset_left = rd.ReadSingle();
+                            float offset_top = rd.ReadSingle();
+                            ushort id = rd.ReadUInt16();
+                            byte page = rd.ReadByte();
+                            byte char_left = rd.ReadByte();
+                            fntwt.WriteLine("char=" + Convert.ToChar(id).ToString() + " id=" + id.ToString() + " left=" + left.ToString() + " right=" + right.ToString() + " top=" + top.ToString() + " bottom=" + bottom.ToString() + " width=" + wd.ToString() + " off_left=" + offset_left.ToString() + " off_top=" + offset_top.ToString() + " page=" + page.ToString() + " char_left=" + char_left.ToString());
+                        }
+                        float next_adv = rd.ReadSingle();
+                        float next_adv_a = rd.ReadSingle();
+                        float next_adv_k = rd.ReadSingle();
+                        fntwt.WriteLine("next_adv=" + next_adv.ToString() + " next_adv_a=" + next_adv.ToString() + " next_adv_k=" + next_adv.ToString());
+                        fntwt.Close();
+                        Console.WriteLine("Done!");
                     }
                     else
                     {
 
                     }
-                    /*var rd = new BinaryReader(File.OpenRead(opf.FileName));
-                    var wt = new BinaryWriter(File.Create("out.dat"));
-                    for(int i = 0; i < rd.BaseStream.Length / 4; i++)
-                    {
-                        byte[] argb = rd.ReadBytes(4);
-                        /*if (argb[0] == 0xFF && argb[1] == 0xFF && argb[2] == 0xFF)
-                        {
-                            wt.Write((byte)0x00);
-                            wt.Write((byte)0x00);
-                            wt.Write((byte)0x00);
-                            wt.Write((byte)0xFF);
-                        }
-                        else*/ /*if (argb[0] == argb[1] && argb[0] != 0xFF)
-                        {
-                            wt.Write(argb[0]);
-                            wt.Write(argb[0]);
-                            wt.Write(argb[0]);
-                            wt.Write((byte)0xFF);
-                        }  
-                        else if(argb[0] != argb[1] && argb[0] == argb[2])
-                        {
-                            wt.Write(argb[0]);
-                            wt.Write(argb[0]);
-                            wt.Write(argb[0]);
-                            wt.Write((byte)0xFF);
-                        }
-                        else if(argb[0] != argb[1] && argb[1] == argb[2])
-                        {
-                            wt.Write(argb[1]);
-                            wt.Write(argb[1]);
-                            wt.Write(argb[1]);
-                            wt.Write((byte)0xFF);
-                        }
-                    }
-                    rd.Close();
-                    wt.Close();*/
                 }
             }
+        }
+
+        public byte[] Dec(byte[] data)
+        {
+            var rd = new BinaryReader(new MemoryStream(data));
+            var ms = new MemoryStream();
+            var wt = new BinaryWriter(ms);
+            for (int i = 0; i < rd.BaseStream.Length / 4; i++)
+            {
+                byte[] argb = rd.ReadBytes(4);
+                if(argb[0] == argb[1])
+                {
+                    wt.Write(argb[0]);
+                    wt.Write(argb[0]);
+                    wt.Write(argb[0]);
+                    wt.Write((byte)0xFF);
+                }
+                else if (argb[0] != argb[1] && argb[0] == argb[2])
+                {
+                    wt.Write(argb[0]);
+                    wt.Write(argb[0]);
+                    wt.Write(argb[0]);
+                    wt.Write((byte)0xFF);
+                }
+                else if (argb[0] != argb[1] && argb[1] == argb[2])
+                {
+                    wt.Write(argb[1]);
+                    wt.Write(argb[1]);
+                    wt.Write(argb[1]);
+                    wt.Write((byte)0xFF);
+                }
+            }
+            return ms.ToArray();
         }
     }
 }
