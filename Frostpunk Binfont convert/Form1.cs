e78be90d217e5eb4449ebd896fea8db7cd3b92f3
Son Hoang <haitac865@gmail.com>
2020-09-18 11:52:15 +0700

    comm

--- Frostpunk Binfont convert/Form1.cs
+++ Frostpunk Binfont convert/Form1.cs
@@ -27,80 +27,12 @@ namespace Frostpunk_Binfont_convert
             {
                 if (!string.IsNullOrEmpty(opf.FileName))
                 {
-                    string pth = Path.GetDirectoryName(opf.FileName);
-                    string filename = Path.GetFileNameWithoutExtension(opf.FileName);
-                    if(Path.GetExtension(opf.FileName) == ".binfont")
-                    {
-                        byte[] hexData = {
-                            0x44, 0x44, 0x53, 0x20, 0x7C, 0x00, 0x00, 0x00, 0x0F, 0x10, 0x02, 0x00 };
-                        byte[] nextData = {
-                            0x01, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00,
-                            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-                            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-                            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x20, 0x00, 0x00, 0x00,
-                            0x41, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x20, 0x00, 0x00, 0x00, 0x00, 0x00, 0xFF, 0x00,
-                            0x00, 0xFF, 0x00, 0x00, 0xFF, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xFF, 0x00, 0x10, 0x00, 0x00,
-                            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
-                        };
-
-                        var rd = new BinaryReader(File.OpenRead(opf.FileName));
-                        int width = 4096;
-                        int magic = rd.ReadInt32();
-                        int ver = rd.ReadInt32();
-                        int texs = rd.ReadInt32();
-                        byte[] height = rd.ReadBytes(texs * 4);
-                        Console.WriteLine("Version {0} , Textures {1}", ver, texs);
-                        var ms = new BinaryReader(new MemoryStream(height));
-                        for (int i = 0; i < texs; i++)
-                        {
-                            int h = ms.ReadInt32();
-                            int Size = width * h * 4;
-                            byte[] outbuf = Dec(rd.ReadBytes(Size));
-                            Console.WriteLine("Dump file {0}", pth + "\\" + filename +"_tex_" + i.ToString() + ".dds");
-                            var wt = new BinaryWriter(File.Create(pth + "\\" + filename + "_tex_" + i.ToString() + ".dds"));
-                            wt.Write(hexData);
-                            wt.Write(h);
-                            wt.Write(width);
-                            wt.Write(Size);
-                            wt.Write(nextData);
-                            wt.Write(outbuf);
-                            wt.Flush();
-                            wt.Close();
-                            Console.WriteLine("Convert Done!");
-                        }
-                        Console.WriteLine("Dump fnt {0}", pth + "\\" + filename + "_table.fnt");
-                        int count = rd.ReadInt32();
-                        StreamWriter fntwt = new StreamWriter(File.Create(pth + "\\" + filename + "_table.fnt"), Encoding.UTF8);
-                        for(int i = 0; i < count; i++)
-                        {
-                            float left = rd.ReadSingle();
-                            float right = rd.ReadSingle();
-                            float top = rd.ReadSingle();
-                            float bottom = rd.ReadSingle();
-                            float wd = rd.ReadSingle();
-                            float offset_left = rd.ReadSingle();
-                            float offset_top = rd.ReadSingle();
-                            ushort id = rd.ReadUInt16();
-                            byte page = rd.ReadByte();
-                            byte char_left = rd.ReadByte();
-                            fntwt.WriteLine("char=" + Convert.ToChar(id).ToString() + " id=" + id.ToString() + " left=" + left.ToString() + " right=" + right.ToString() + " top=" + top.ToString() + " bottom=" + bottom.ToString() + " width=" + wd.ToString() + " off_left=" + offset_left.ToString() + " off_top=" + offset_top.ToString() + " page=" + page.ToString() + " char_left=" + char_left.ToString());
-                        }
-                        float next_adv = rd.ReadSingle();
-                        float next_adv_a = rd.ReadSingle();
-                        float next_adv_k = rd.ReadSingle();
-                        fntwt.WriteLine("next_adv=" + next_adv.ToString() + " next_adv_a=" + next_adv.ToString() + " next_adv_k=" + next_adv.ToString());
-                        fntwt.Close();
-                        Console.WriteLine("Done!");
-                    }
-                    else
-                    {
-
-                    }
+                    MainEvent(opf.FileName);
                 }
             }
         }
 
-        public byte[] Dec(byte[] data)
+        public static byte[] Dec(byte[] data)
         {
             var rd = new BinaryReader(new MemoryStream(data));
             var ms = new MemoryStream();
@@ -131,6 +63,107 @@ namespace Frostpunk_Binfont_convert
                 }
             }
             return ms.ToArray();
+        }
+
+        public static void MainEvent(string path)
+        {
+            string pth = Path.GetDirectoryName(path);
+            string filename = Path.GetFileNameWithoutExtension(path);
+            if (Path.GetExtension(path) == ".binfont")
+            {
+                try
+                {
+                    byte[] hexData = {
+                            0x44, 0x44, 0x53, 0x20, 0x7C, 0x00, 0x00, 0x00, 0x0F, 0x10, 0x02, 0x00 };
+                    byte[] nextData = {
+                            0x01, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00,
+                            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+                            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+                            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x20, 0x00, 0x00, 0x00,
+                            0x41, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x20, 0x00, 0x00, 0x00, 0x00, 0x00, 0xFF, 0x00,
+                            0x00, 0xFF, 0x00, 0x00, 0xFF, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xFF, 0x00, 0x10, 0x00, 0x00,
+                            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+                        };
+
+                    var rd = new BinaryReader(File.OpenRead(path));
+                    int width = 4096;
+                    int magic = rd.ReadInt32();
+                    int ver = rd.ReadInt32();
+                    int texs = rd.ReadInt32();
+                    byte[] height = rd.ReadBytes(texs * 4);
+                    Console.WriteLine("Version {0} , Textures {1}", ver, texs);
+                    var ms = new BinaryReader(new MemoryStream(height));
+                    for (int i = 0; i < texs; i++)
+                    {
+                        int h = ms.ReadInt32();
+                        int Size = width * h * 4;
+                        byte[] outbuf = Dec(rd.ReadBytes(Size));
+                        Console.WriteLine("Dump file {0}", pth + "\\" + filename + "_tex_" + i.ToString() + ".dds");
+                        var wt = new BinaryWriter(File.Create(pth + "\\" + filename + "_tex_" + i.ToString() + ".dds"));
+                        wt.Write(hexData);
+                        wt.Write(h);
+                        wt.Write(width);
+                        wt.Write(Size);
+                        wt.Write(nextData);
+                        wt.Write(outbuf);
+                        wt.Flush();
+                        wt.Close();
+                        Console.WriteLine("Convert Done!");
+                    }
+                    Console.WriteLine("Dump fnt {0}", pth + "\\" + filename + "_tex_.fnt");
+                    int count = rd.ReadInt32();
+                    StreamWriter fntwt = new StreamWriter(File.Create(pth + "\\" + filename + "_tex_.fnt"), Encoding.UTF8);
+                    for (int i = 0; i < count; i++)
+                    {
+                        float left = rd.ReadSingle();
+                        float right = rd.ReadSingle();
+                        float top = rd.ReadSingle();
+                        float bottom = rd.ReadSingle();
+                        float wd = rd.ReadSingle();
+                        float offset_left = rd.ReadSingle();
+                        float offset_top = rd.ReadSingle();
+                        ushort id = rd.ReadUInt16();
+                        byte page = rd.ReadByte();
+                        byte char_left = rd.ReadByte();
+                        fntwt.WriteLine("char=" + Convert.ToChar(id).ToString() + " id=" + id.ToString() + " left=" + left.ToString() + " right=" + right.ToString() + " top=" + top.ToString() + " bottom=" + bottom.ToString() + " width=" + wd.ToString() + " off_left=" + offset_left.ToString() + " off_top=" + offset_top.ToString() + " page=" + page.ToString() + " char_left=" + char_left.ToString());
+                    }
+                    float next_adv = rd.ReadSingle();
+                    float next_adv_a = rd.ReadSingle();
+                    float next_adv_k = rd.ReadSingle();
+                    fntwt.WriteLine("next_adv=" + next_adv.ToString() + " next_adv_a=" + next_adv.ToString() + " next_adv_k=" + next_adv.ToString());
+                    fntwt.Close();
+                    Console.WriteLine("Done!");
+                }
+                catch
+                {
+                    MessageBox.Show("File format isn't support!");
+                    Console.WriteLine("Wrong format file!");
+                }
+            }
+            else if(Path.GetExtension(path) == ".fnt")
+            {
+                try
+                {
+                    var ms = new MemoryStream();
+                    var mswt = new BinaryWriter(ms);
+                    string[] lines = File.ReadAllLines(path);
+                    foreach (string line in lines)
+                    {
+                        mswt.Write(lines.Length - 1);
+
+                    }
+                }
+                catch
+                {
+                    MessageBox.Show("File format isn't support!");
+                    Console.WriteLine("Wrong format file!");
+                }
+            }
+            else
+            {
+                MessageBox.Show("File format isn't support!");
+                Console.WriteLine("Wrong format file!");
+            }
         }
     }
 }
--- Frostpunk Binfont convert/Program.cs
+++ Frostpunk Binfont convert/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Frostpunk_Binfont_convert;
 
 namespace Frostpunk_Binfont_convert
 {
@@ -12,11 +13,20 @@ namespace Frostpunk_Binfont_convert
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main()
+        static void Main(string[] args)
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            if(args.Length == 0)
+            {
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new Form1());
+            }
+            else
+            {
+                Frostpunk_Binfont_convert.Form1.MainEvent(args[0]);
+                Console.WriteLine("Press any button...");
+                Console.ReadKey();
+            }
         }
     }
 }
